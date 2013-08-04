138517d770f4943b2c6154a282ec4bfbf9bea79a
alice <alina.ghergu@gmail.com>
2013-08-04 23:29:50 +0300

    Simple is better than complex.

--- python/koans/python3/koans/about_dice_project.py
+++ python/koans/python3/koans/about_dice_project.py
@@ -14,9 +14,9 @@ class DiceSet:
         return self._values
 
     def roll(self, n):
-        # Needs implementing!
+        random.seed()
         # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = [random.randint(1, 6) for el in range(0,n)]
 
 class AboutDiceProject(Koan):
     def test_can_create_a_dice_set(self):
