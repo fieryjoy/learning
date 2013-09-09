ea30b68aff2611260b2a1e4b6149efbc373b002a
Gratiela Ghergu <alina.ghergu@gmail.com>
2013-09-09 11:35:23 +0300

    Part 4

--- python/django/eris/polls/views.py
+++ python/django/eris/polls/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render_to_response
-from polls.models import Poll
+from django.shortcuts import render_to_response, get_object_or_404
+from polls.models import Poll, Choice
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse 
+from django.template import RequestContext
 
 
 def index(request):
@@ -7,10 +10,25 @@ def index(request):
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
     
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s" % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s" % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s" % poll_id)    
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form        
+        return render_to_response('polls/details.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+            }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))    
--- python/django/eris/settings.py
+++ python/django/eris/settings.py
@@ -110,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/alice/mytemplates'
+    '/local/learning/python/django/eris/mytemplates'
 )
 
 INSTALLED_APPS = (
