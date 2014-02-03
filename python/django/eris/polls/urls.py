3ef9b2e6fc201d3a4f103f7d4605da69c9539f91
justanub <alina.ghergu@gmail.com>
2014-02-03 15:32:21 +0200

    Pick a REST framework for django. Second and final choice: tastypie

--- python/django/eris/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('polls.views',
-                       url(r'^$', 'index'),
-                       url(r'^(?P<poll_id>\d+)/$', 'detail'),
-                       url(r'^(?P<poll_id>\d+)/results/$', 'results'),
-                       url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),                    
-)
-
