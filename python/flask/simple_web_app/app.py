5cb8ae71b95240829dc5c7be0045c1952200e775
fieryjoy <alina.ghergu@gmail.com>
2016-06-03 09:39:44 +0200

    finishing signUp

--- python/flask/simple_web_app/app.py
+++ python/flask/simple_web_app/app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request, json
-app = Flask(__name__)
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
 
+app = Flask(__name__)
+mysql = MySQL()
+
+#MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
+@app.route('/main')
 def main():
     return render_template('index.html')
 
@@ -19,7 +30,16 @@ def signUp():
     _password = request.form['inputPassword']
 
     if _name and _email and _password:
-        return json.dumps({'html': '<span>All fields good !!</span>'})
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        _hashed_password = generate_password_hash(_password)
+        cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+        data = cursor.fetchall()
+        if len(data) is 0:
+            conn.commit()
+            return json.dumps({'message': 'User created successfully'})
+        else:
+            return json.dumps({'error': str(data[0])})
     else:
         return json.dumps({'html': '<span>Enter the required fields</span>'})
 
