44798d1c78c19d34735fd2e1c2edbd404a8c2548
fieryjoy <alina.ghergu@gmail.com>
2016-06-06 17:06:02 +0200

    update and delete a wish

---
+++ python/flask/simple_web_app/sql/sp_deleteWish.sql
@@ -0,0 +1,13 @@
+USE `BucketList`;
+DROP procedure IF EXISTS `sp_deleteWish`;
+DELIMITER $$
+USE `BucketList`$$
+CREATE PROCEDURE `sp_deleteWish` (
+IN p_wish_id bigint,
+IN p_user_id bigint
+)
+BEGIN
+delete from tbl_wish where wish_id = p_wish_id and wish_user_id = p_user_id;
+END$$
+
+DELIMITER ;
