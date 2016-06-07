4efe3a6318f5c0ef02e13621290061efc6aa39cc
fieryjoy <alina.ghergu@gmail.com>
2016-06-07 10:29:18 +0200

    add pagination

--- python/flask/simple_web_app/app.py
+++ python/flask/simple_web_app/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.secret_key = 'why would I tell you my secret key?'
 mysql = MySQL()
 
+pageLimit = 2
+
 #MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
@@ -119,33 +121,45 @@ def addWish():
         cursor.close()
         conn.close()
 
-
-@app.route('/getWish')
+@app.route('/getWish', methods=['POST'])
 def getWish():
     try:
         if session.get('user'):
             _user = session.get('user')
+            _limit = pageLimit
+            _offset = request.form['offset']
+            _total_records = 0
 
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.callproc('sp_GetWishByUser', (_user,))
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('sp_GetWishByUser', (_user, _limit, _offset, _total_records))
+
             wishes = cursor.fetchall()
+            cursor.close()
 
+            cursor = con.cursor()
+
+            cursor.execute('SELECT @_sp_GetWishByUser_3;')
+
+            outParam = cursor.fetchall()
+
+            response = []
             wishes_dict = []
             for wish in wishes:
                 wish_dict = {
                     'Id': wish[0],
                     'Title': wish[1],
                     'Description': wish[2],
-                    'Date': wish[4]
-                }
+                    'Date': wish[4]}
                 wishes_dict.append(wish_dict)
-            return json.dumps(wishes_dict)
+            response.append(wishes_dict)
+            response.append({'total': outParam[0][0]})
+
+            return json.dumps(response)
         else:
-            return render_template('error.html', error = 'Unauthorized Access')
+            return render_template('error.html', error='Unauthorized Access')
     except Exception as e:
         return render_template('error.html', error=str(e))
-
 
 @app.route('/getWishById', methods=['POST'])
 def getWishById():
--- python/flask/simple_web_app/sql/sp_GetWishByUser.sql
+++ python/flask/simple_web_app/sql/sp_GetWishByUser.sql
@@ -3,11 +3,20 @@ DROP procedure IF EXISTS `sp_GetWishByUser`;
 
 DELIMITER $$
 USE `BucketList`$$
-CREATE PROCEDURE `sp_GetWishByUser` (
-IN p_user_id bigint
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetWishByUser`(
+IN p_user_id bigint,
+IN p_limit int,
+IN p_offset int,
+out p_total bigint
 )
 BEGIN
-    select * from tbl_wish where wish_user_id = p_user_id;
+
+    select count(*) into p_total from tbl_wish where wish_user_id = p_user_id;
+
+    SET @t1 = CONCAT( 'select * from tbl_wish where wish_user_id = ', p_user_id, ' order by wish_date desc limit ',p_limit,' offset ',p_offset);
+    PREPARE stmt FROM @t1;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
 END$$
 
 DELIMITER ;
